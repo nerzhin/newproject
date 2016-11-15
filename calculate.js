b1c4d5a43a9c9212ce46f194a63e9c3de360c56a
nerzhin <grach.scheglov@gmail.com>
2016-11-15 20:29:08 +0200

    beautified

--- calculate.js
+++ calculate.js
@@ -1,25 +1,25 @@
 "use strict";
 
 function Calculator() {
-  var methods = {
-    '+': function(a,b) {
-      return a + b;
-    },
-    '-': function(a,b) {
-      return a - b;
+    var methods = {
+        '+': function(a, b) {
+            return a + b;
+        },
+        '-': function(a, b) {
+            return a - b;
+        }
     }
-  }
 
-  this.calculate = function(string) {
-    var split = string.split(' ');
-    var a = split[0];
-    var b = split[2];
-    var op = split[1];
+    this.calculate = function(string) {
+        var split = string.split(' ');
+        var a = split[0];
+        var b = split[2];
+        var op = split[1];
 
-    return methods[op](+a, +b);
-  };
+        return methods[op](+a, +b);
+    };
 
-  this.addMethod = function(name,func) {
-    methods[name] = func;
-  };
+    this.addMethod = function(name, func) {
+        methods[name] = func;
+    };
 }
